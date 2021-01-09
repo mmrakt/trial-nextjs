f4745d183e2518676c9ddedc4ebbf832c08a736e
mmrakt <mimuraakkun3@gmail.com>
2021-01-09 11:59:10 +0900

    functionsの関数を整理

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,13 @@
 const fbFunc = require('firebase-functions')
 const nodemailer = require('nodemailer')
-const gmailEmail = fbFunc.config().gmail.email
-const adminEmail = fbFunc.config().admin.email
-const adminPassword = fbFunc.config().admin.password
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = fbFunc.https.onRequest(
-    (request: any, response: any) => {
-        fbFunc.logger.info('Hello logs!', { structuredData: true })
-        response.send('Hello from Firebase!')
-    }
-)
 
-exports.helloOnRequest = fbFunc.https.onRequest(
-    (request: any, response: any) => {
-        response.send('Hello from Firebase!')
-    }
-)
+const gmailEmail = fbFunc.config().gmail.email
+const adminEmail = fbFunc.config().admin.email
+const adminPassword = fbFunc.config().admin.password
 
 const mailTransport = nodemailer.createTransport({
     service: 'gmail',
@@ -34,21 +23,21 @@ const toUserMailContent = (data: any) => {
     本メールにお心当たりのない場合は無視してください。`
 }
 
-exports.sendMail = fbFunc.https.onCall(async (data: any, context: any) => {
-    const toUserMail = {
-        from: adminEmail,
-        to: gmailEmail,
-        subject: 'パスワード更新完了',
-        text: toUserMailContent(data),
-    }
+exports.sendMailCompleteUpdatePassword = fbFunc.https.onCall(
+    async (data: any) => {
+        const toUserMail = {
+            from: adminEmail,
+            to: gmailEmail,
+            subject: 'パスワード更新完了',
+            text: toUserMailContent(data),
+        }
 
-    try {
-        await mailTransport.sendMail(toUserMail)
-    } catch (error) {
-        console.log(error)
+        try {
+            await mailTransport.sendMail(toUserMail)
+        } catch (error) {
+            console.log(error)
+        }
     }
-})
+)
 
-exports.helloOnCall = fbFunc.https.onCall((data: any, context: any) => {
-    return 'Hello OnCall!'
-})
+export {}
