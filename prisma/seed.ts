230f334980598aae12336e942711e6601c0c751d
mmrakt <mimuraakkun3@gmail.com>
2021-01-28 00:57:00 +0900

    userテーブル用のseed追加

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import prisma from '../src/lib/prisma'
+
+const seed = async (): Promise<void> => {
+    await prisma.user.upsert({
+        where: { id: 'testuser01' },
+        update: {},
+        create: {
+            id: 'testuser01',
+            name: 'testuser01',
+            profile: 'testtest',
+            avatarUrl: '',
+        },
+        select: { id: true, name: true, profile: true },
+    })
+    await prisma.user.upsert({
+        where: { id: 'testuser02' },
+        update: {},
+        create: {
+            id: 'testuser02',
+            name: 'testuser02',
+            profile: 'testtesttest',
+            avatarUrl: '',
+        },
+        select: { id: true, name: true, profile: true },
+    })
+    await prisma.user.upsert({
+        where: { id: 'testuser03' },
+        update: {},
+        create: {
+            id: 'testuser03',
+            name: 'testuser03',
+            profile: 'testtesttest',
+            avatarUrl: '',
+        },
+        select: { id: true, name: true, profile: true },
+    })
+}
+
+seed()
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect
+    })
