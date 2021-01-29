d05f467393f99d5f2576eef57b6f9d991627d44b
mmrakt <mimuraakkun3@gmail.com>
2021-01-29 22:57:26 +0900

    つぶやき後ににゅうりょフォームを空に

--- src/pages/tweet/TweetForm.tsx
+++ src/pages/tweet/TweetForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import {} from '@material-ui/core'
 import Button from '../../components/Button'
 import { useMutation, useQueryClient } from 'react-query'
@@ -6,6 +6,7 @@ import { useMutation, useQueryClient } from 'react-query'
 const TweetForm = (): React.ReactElement => {
     const [content, setContent] = useState<string>('')
     const queryClient = useQueryClient()
+    const inputRef = useRef(null)
 
     const { mutate } = useMutation(
         () => {
@@ -25,16 +26,24 @@ const TweetForm = (): React.ReactElement => {
     const onTweet = (event) => {
         event.preventDefault()
         mutate()
+        inputRef.current.value = ''
+        setContent('')
     }
 
     return (
-        <div>
+        <div className="border-solid border-2 py-4 px-4">
             <textarea
                 className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                 placeholder="今なにしてる？"
                 onChange={(event) => setContent(event.target.value)}
+                ref={inputRef}
             ></textarea>
-            <Button text="つぶやく" onClickEvent={(event) => onTweet(event)} />
+            <Button
+                className="float-right"
+                text="つぶやく"
+                onClickEvent={(event) => onTweet(event)}
+            />
+            <p className="clear-right"></p>
         </div>
     )
 }
