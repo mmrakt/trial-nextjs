3c34b1fc8148ca04ea8cfcafb1c701089db3c9c7
mmrakt <mimuraakkun3@gmail.com>
2021-01-29 22:56:38 +0900

    メモ削除

--- src/pages/tweet/TweetList.tsx
+++ src/pages/tweet/TweetList.tsx
@@ -3,26 +3,6 @@ import {} from '@material-ui/core'
 import { useQuery } from 'react-query'
 import { Tweet as ITweet } from '@prisma/client'
 import TweetItem from './TweetItem'
-//import { GetServerSideProps } from 'next'
-
-// type IProps = {
-//     tweetList: {
-//         id: number
-//         content: string
-//     }[]
-// }
-
-//NOTE: こうやって書く
-// export const getServerSideProps: GetServerSideProps<IProps> = async (ctx) => {
-//     const tweetList = await prisma.tweet.findMany({
-//         where: {},
-//     })
-//     return {
-//         props: {
-//             tweetList,
-//         },
-//     }
-// }
 
 const TweetList = (): React.ReactElement => {
     const { data: tweetList, isLoading } = useQuery<ITweet[]>(
@@ -37,13 +17,15 @@ const TweetList = (): React.ReactElement => {
     if (isLoading) return <span>Loading...</span>
 
     return (
-        <div>
+        <div className="border-solid border-2">
             {tweetList && (
-                <ul>
-                    {tweetList.map((tweet) => (
-                        <TweetItem tweet={tweet} key={tweet.id} />
-                    ))}
-                </ul>
+                <>
+                    <ul className="divide-y-2 divide-black divide-opacity-10">
+                        {tweetList.map((tweet) => (
+                            <TweetItem tweet={tweet} key={tweet.id} />
+                        ))}
+                    </ul>
+                </>
             )}
         </div>
     )
