4e0897a75d3ea006e6ff8d4fe459c347376fc1b4
mmrakt <mimuraakkun3@gmail.com>
2021-01-29 22:56:21 +0900

    tweetItemを充実させた

--- src/pages/tweet/TweetItem.tsx
+++ src/pages/tweet/TweetItem.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
-import {} from '@material-ui/core'
 import { Tweet } from '@prisma/client'
+import dayjs from 'dayjs'
+import Image from 'next/image'
+import { useQuery } from 'react-query'
+import { User as IUser } from '@prisma/client'
 
 const TweetItem = ({ tweet }: { tweet: Tweet }): React.ReactElement => {
-    return <li key={tweet.id}>{tweet.content}</li>
+    const { data: user, isLoading } = useQuery<IUser>('user', async () => {
+        const res = await fetch(`/api/user/fetch/?id=${tweet.userId}`)
+        return res.json()
+    })
+    if (isLoading) return <span>Loading...</span>
+
+    return (
+        <div className="flex my-4 mx-2 py-2 px-2">
+            <Image
+                src={user.avatarUrl ? user.avatarUrl : '/avatar.png'}
+                alt="アバター画像"
+                width={50}
+                height={50}
+                className="rounded-full mx-2"
+            />
+            <div className="mx-2">
+                <div className="text-base">
+                    <strong className="mx-1">{user.name}</strong>
+                    <span className="mx-1">@{user.id}</span>
+                    {dayjs(tweet.createdAt).format('YYYY/MM/DD HH:mm:ss')}
+                </div>
+                <div className="text-xl">{tweet.content}</div>
+            </div>
+        </div>
+    )
 }
 
 export default TweetItem
